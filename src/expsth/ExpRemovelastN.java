3b154ce4473aec3c651eda60b1273fed1cf7868c
thaithien <ttpro1995@gmail.com>
2019-08-12 10:49:11 +0700

    add some file

---
+++ src/expsth/ExpRemovelastN.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package expsth;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author thient <thient@vng.com.vn>
+ */
+public class ExpRemovelastN {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 3, 2, 4, 1, 2, 3, 4 ,5);
+        
+    }
+}
